2b95aea852d50f535f72717269674bbec1a718e2
Codian <44289933+xxcodianxx@users.noreply.github.com>
2023-12-17 21:04:44 +1030

    Initial commit

---
+++ PerPlayerLoot/FakeChestDatabase.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Terraria;
+using TShockAPI;
+using TerrariaApi.Server;
+using System.IO;
+using System.IO.Streams;
+
+using Newtonsoft.Json;
+using System.Data.SQLite;
+using Newtonsoft.Json.Bson;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PerPlayerLoot
+{
+    // barebones class representing item data which can be deserialized from json
+    public class JItem
+    {
+        public int id { get; set; }
+        public int stack { get; set; }
+        public byte prefix { get; set; }
+    }
+
+
+    // database of FakeChest's
+    public class FakeChestDatabase
+    {
+        // Map { UUID: { ChestID: Chest } }
+        public static Dictionary<string, Dictionary<int, Chest>> fakeChestsMap = new Dictionary<string, Dictionary<int, Chest>> { };
+
+        public static HashSet<(int, int)> playerPlacedChests = new HashSet<(int, int)>(); // tile x, y of player placed chests
+
+        private static string connString = "Data Source=perplayerloot.sqlite;Version=3;";
+
+        public FakeChestDatabase() { }
+
+        public void Initialize()
+        {
+            CreateTables();
+            LoadFakeChests();
+        }
+
+        public void CreateTables()
+        {
+            TSPlayer.Server.SendInfoMessage("Setting up per-player chests database...");
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                conn.Open();
+
+                string sql = @"
+                    CREATE TABLE IF NOT EXISTS chests (
+                        id INTEGER NOT NULL,
+                        playerUuid TEXT NOT NULL,
+                        x INTEGER NOT NULL,
+                        y INTEGER NOT NULL,
+                        items BLOB NOT NULL,
+                        PRIMARY KEY (id, playerUuid)
+                    );
+
+                    CREATE TABLE IF NOT EXISTS placed (
+                        x INTEGER NOT NULL,
+                        y INTEGER NOT NULL,
+                        PRIMARY KEY (x, y)
+                    );
+                ";
+
+                using (var cmd = new SQLiteCommand(sql, conn)) 
+                    cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        public void LoadFakeChests()
+        {
+            TSPlayer.Server.SendInfoMessage("Loading per-player loot chest inventories...");
+            int count = 0;
+
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                conn.Open();
+
+                // load loot chests
+                using (var cmd = new SQLiteCommand("SELECT id, playerUuid, x, y, items FROM chests;", conn))
+                {
+                    SQLiteDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string playerUuid = Convert.ToString(reader["playerUuid"]);
+                        int chestId = Convert.ToInt32(reader["id"]);
+
+                        // get the items list
+                        List<Item> items = new List<Item>();
+
+                        // read blob from column
+                        MemoryStream itemsRaw = new MemoryStream((byte[]) reader["items"]);
+                        // deserialize with bson
+                        using (var br = new BsonReader(itemsRaw))
+                        {
+                            br.ReadRootValueAsArray = true;
+                            
+                            // do the actual deserialization
+                            var jItems = (new JsonSerializer()).Deserialize<IList<JItem>>(br);
+
+                            // convert each JItem to a real Item
+                            foreach (var jItem in jItems)
+                            {
+                                if (jItem == null)
+                                {
+                                    items.Add(null);
+                                    continue;
+                                }
+
+                                var item = new Item();
+                                item.netDefaults(jItem.id);
+                                item.stack = jItem.stack;
+                                item.prefix = jItem.prefix;
+
+                                items.Add(item);
+                            }
+                        }
+
+                        Chest chest = new Chest(); // construct a terraria chest
+                        chest.x = Convert.ToInt32(reader["x"]);
+                        chest.y = Convert.ToInt32(reader["y"]);
+                        chest.item = items.ToArray();
+
+                        // save it in the fake chest map
+                        var playerChests = fakeChestsMap.GetValueOrDefault(playerUuid, new Dictionary<int, Chest>());
+                        fakeChestsMap[playerUuid] = playerChests;
+
+                        fakeChestsMap[playerUuid][chestId] = chest;
+
+                        count++;
+                    }
+                }
+
+                // load tile exclusions
+                using (var cmd = new SQLiteCommand("SELECT x, y FROM placed;", conn))
+                {
+                    SQLiteDataReader reader = cmd.ExecuteReader();
+
+                    playerPlacedChests.Clear();
+
+                    while (reader.Read())
+                    {
+                        int x = Convert.ToInt32(reader["x"]);
+                        int y = Convert.ToInt32(reader["y"]);
+
+                        playerPlacedChests.Add((x, y));
+                    }
+                }
+            }
+
+            TSPlayer.Server.SendSuccessMessage($"Loaded {count} loot chest inventories, {playerPlacedChests.Count} player-placed chests.");
+        }
+
+        public void SaveFakeChests()
+        {
+            TSPlayer.Server.SendInfoMessage("Saving per-player loot chest inventories...");
+            int count = 0;
+
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                conn.Open();
+
+                foreach (KeyValuePair<string, Dictionary<int, Chest>> playerEntry in fakeChestsMap)
+                {
+                    string playerUuid = playerEntry.Key;
+                    var playerChests = playerEntry.Value;
+
+                    foreach (KeyValuePair<int, Chest> chestEntry in playerChests)
+                    {
+                        int chestId = chestEntry.Key;
+                        var chest = chestEntry.Value;
+
+                        List<JItem> jItems = new List<JItem>(chest.item.Length);
+
+                        foreach (var item in chest.item)
+                        {
+                            var jItem = new JItem();
+
+                            jItem.id = item.type;
+                            jItem.stack = item.stack;
+                            jItem.prefix = item.prefix;
+
+                            jItems.Add(jItem);
+                        }
+
+                        MemoryStream itemsMs = new MemoryStream();
+                        using (var writer = new BsonWriter(itemsMs))
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            serializer.Serialize(writer, jItems);
+                        }
+
+                        var sql = @"REPLACE INTO chests (id, playerUuid, x, y, items) VALUES (@id, @playerUuid, @x, @y, @items);";
+
+                        using (var cmd = new SQLiteCommand(sql, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@id", chestId);
+                            cmd.Parameters.AddWithValue("@playerUuid", playerUuid);
+                            cmd.Parameters.AddWithValue("@x", chest.x);
+                            cmd.Parameters.AddWithValue("@y", chest.y);
+                            cmd.Parameters.AddWithValue("@items", itemsMs.ToArray());
+
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        count++;
+                    }
+                }
+
+                foreach ((int x, int y) in playerPlacedChests)
+                {
+                    var sql = @"REPLACE INTO placed (x, y) VALUES (@x, @y);";
+
+                    using (var cmd = new SQLiteCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@x", x);
+                        cmd.Parameters.AddWithValue("@y", y);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            TSPlayer.Server.SendSuccessMessage($"Saved {count} loot chest inventories, {playerPlacedChests.Count} player-placed chests.");
+        }
+
+        public Chest GetOrCreateFakeChest(int chestId, string playerUuid)
+        {
+            var playerChests = fakeChestsMap.GetValueOrDefault(playerUuid, new Dictionary<int, Chest>());
+            fakeChestsMap[playerUuid] = playerChests;
+
+            if (!playerChests.ContainsKey(chestId))
+            {
+                
+                Chest realChest = Main.chest[chestId];
+
+                // copy the chest data from the real untouched chest
+                Chest fakeChest = new Chest();
+                fakeChest.x = realChest.x;
+                fakeChest.y = realChest.y;
+                realChest.item.CopyTo(fakeChest.item, 0);
+
+                // save it in the fake chest list
+                fakeChestsMap[playerUuid][chestId] = fakeChest;
+
+                // save the fake chests list to disk
+                SaveFakeChests();
+
+                return fakeChest;
+            }
+
+            return playerChests[chestId];
+        }
+
+        public void SetChestPlayerPlaced(int tileX, int tileY)
+        {
+            playerPlacedChests.Add((tileX, tileY));
+        }
+
+        public bool IsChestPlayerPlaced(int tileX, int tileY)
+        {
+            return playerPlacedChests.Contains((tileX, tileY));
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,61 @@
+![Per Player Loot](.github/PerPlayerLoot.png)
+
+# Terraria Per Player Loot
+A [TShock](https://github.com/Pryaxis/TShock) server plugin which makes
+naturally spawned loot chests have a separate inventory for each player on your
+server. Every player that finds a chest can loot it for themselves, even if it
+has been looted by someone else before.
+
+## The Problem
+Terraria multiplayer has one big issue - the world you play on is finite, and
+there are only so many items and resources to go around. 
+
+When you start a new world to play on with your friends, the few people who
+choose to go surface exploring or caving are likely to find most of the chests,
+and, as a result, most of the early game loot.
+
+This is not a problem with bosses in Expert or Master mode, because they drop a
+loot bag for each player that damaged them, and everyone can be happy.
+
+This plugin makes it much more viable to run large Terraria multiplayer servers,
+as there is always an incentive for exploring and caving - the loot is still
+there for you to find!
+
+## Installation
+1. Copy `PerPlayerLoot.dll` into the `ServerPlugins` directory of your TShock
+   server.
+2. Copy `System.Data.SQLite.dll` into the `bin` directory of your TShock server.
+3. Start a new world from scratch (yes, this is important, read below) and play!
+
+## How it Works
+This plugin aims to not modify the server-side chest state
+(`Terraria.Main.chest`) on chest interactions, in the hope that it can be as
+least destructive to world save data as possible. 
+
+It works by intercepting the `ChestPlace`, `ChestOpen` and `ChestItem`
+packets and spoofing the contents of a loot chest by sending carefully crafted
+`ChestItem` packets from an internal database. 
+
+The per-player loot data is written on disk to the SQLite file
+`perplayerloot.sqlite` on every world save, alongside a list of all
+player-placed chest X and Y coordinates. **If your player-placed chests are not
+in this exclusion list, they will be treated as world-generated loot chests.**
+
+This means that In order for player-placed chests to be unaffected, **you need
+to have the plugin installed from the very beginning of your server.** If you
+install it halfway through a playthrough, all chests in the world will be
+treated as if they were generated and will duplicate their inventory contents
+for each player.
+
+The only time when the `Main.chest` array is modified is when a loot chest is
+possibly broken (which should rarely happen). In that case, the real chest in
+`Main.chest` has its items zeroed.
+
+## Debug Commands
+- `/ppltoggle` - Toggle the plugin packet hooks globally. **WARNING:** using
+  this command is unsupported and can lead to desynchronization of the
+  `Main.chest` array and the internal plugin state. 
+  
+  Debug use only! When in a disabled state, any chests you place will become
+  loot chests, and any chest inventory accessed will be its **real inventory**,
+  not a per-player instanced one!
